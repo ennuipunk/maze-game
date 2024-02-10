d287e40c4db5ce40477a65ed2f5648089666766e
Kenny Love <kennylove@Kennys-MacBook-Air.local>
2024-02-10 16:26:14 +0000

    player can move around screen

---
+++ index.js
@@ -0,0 +1,52 @@
+function numberFromPixels(pixelsString) {
+  var numberString = pixelsString.slice(0, -2);
+  var number = parseInt(numberString);
+  return number;
+}
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'd') {
+        var square = document.getElementById('player');
+        var leftpx = square.style.left;
+        if(leftpx){
+            var currentLeft = numberFromPixels(leftpx);
+            var newleft = currentLeft + 20;
+            square.style.left = newleft + 'px';
+        } else {
+            square.style.left = "20px";
+        }
+    }
+    if (event.key === 'a') {
+        var square = document.getElementById('player');
+        var leftpx = square.style.left;
+        if(leftpx){
+            var currentLeft = numberFromPixels(leftpx);
+            var newleft = currentLeft - 20;
+            square.style.left = newleft + 'px';
+        } else {
+            square.style.left = "20px";
+        }
+    }
+    if (event.key === 's') {
+        var square = document.getElementById('player');
+        var toppx = square.style.top;
+        if(toppx){
+            var currenttop = numberFromPixels(toppx);
+            var newtop = currenttop + 20;
+            square.style.top = newtop + 'px';
+        } else {
+            square.style.top = "20px";
+        }
+    }
+    if (event.key === 'w') {
+        var square = document.getElementById('player');
+        var toppx = square.style.top;
+        if(toppx){
+            var currenttop = numberFromPixels(toppx);
+            var newtop = currenttop - 20;
+            square.style.top = newtop + 'px';
+        } else {
+            square.style.top = "20px";
+        }
+    }
+  });
